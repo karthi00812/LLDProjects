8a1b8b1969016a984d3566ce4f00cfc0a155461c
karathikeyan <karthikeyan@banfico.com>
2024-10-06 20:47:29 +0530

    Modal mapper converter, propertyMap.

---
+++ src/main/java/org/example/UserDTO.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class UserDTO {
+    private Long id;
+    private String fullName;
+    private String emailAddress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                '}';
+    }
+}
